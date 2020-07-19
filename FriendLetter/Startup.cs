d81a84deaa39e5e6c58f61b0845df8e98781a48c
Ian Scott <chesnekov@gmail.com>
2020-07-19 09:46:30 -0700

    Updates missinx exe reference projects csproj file, updates Startup.cs with new ways of setting up running and routing configurations etc with netcore app 3.1

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -16,26 +16,54 @@ namespace FriendLetter
       Configuration = builder.Build();
     }
 
-    public IConfigurationRoot configuration { get; }
+    public IConfigurationRoot Configuration { get; }
 
     public void ConfigureServices (IServiceCollection services)
     {
-      services.AddMvc();
+      // New!
+      services.AddControllers();
+      services.AddRazorPages();
+
+      // Old
+      //services.AddMvc();
+
     }
 
     public void Configure (IApplicationBuilder app)
     {
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-          name: "default",
-          template: "{controller=Home}/{action=Index}{id?}");
-      });
+    app.UseStaticFiles();
 
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Hello World!");
-      });
+    // Runs matching. An endpoint is selected and set on the HttpContext if a match is found.
+    app.UseRouting(); 
+
+    // Middleware that run after routing occurs. Usually the following appear here:
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseCors();
+    // These middleware can take different actions based on the endpoint.
+
+    // Executes the endpoint that was selected by routing.
+    app.UseEndpoints(endpoints =>
+    {
+        // Mapping of endpoints goes here:
+        endpoints.MapControllers();
+        endpoints.MapRazorPages();
+    });
+
+    // Middleware here will only run if nothing was matched.
+
+    //Old stuff!
+      // app.UseMvc(routes =>
+      // {
+      //   routes.MapRoute(
+      //     name: "default",
+      //     template: "{controller=Home}/{action=Index}{id?}");
+      // });
+
+      // app.Run(async (context) =>
+      // {
+      //   await context.Response.WriteAsync("Hello World!");
+      // });
     }
   }
 }
