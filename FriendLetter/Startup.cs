e3a245ab27afe28a5afe947708663bf79b2d5179
Ian Scott <chesnekov@gmail.com>
2020-07-19 10:26:02 -0700

    reverts project to dotnet corre 2.2 to more easily follow along with the lesson

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -20,45 +20,20 @@ namespace FriendLetter
 
     public void ConfigureServices (IServiceCollection services)
     {
-      // New!
-      services.AddControllers();
-      services.AddRazorPages();
 
-      // Old
-      //services.AddMvc();
+      //Old
+      services.AddMvc();
 
     }
 
     public void Configure (IApplicationBuilder app)
     {
-    app.UseStaticFiles();
-
-    // Runs matching. An endpoint is selected and set on the HttpContext if a match is found.
-    app.UseRouting(); 
-
-    // Middleware that run after routing occurs. Usually the following appear here:
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.UseCors();
-    // These middleware can take different actions based on the endpoint.
-
-    // Executes the endpoint that was selected by routing.
-    app.UseEndpoints(endpoints =>
-    {
-        // Mapping of endpoints goes here:
-        endpoints.MapControllers();
-        endpoints.MapRazorPages();
-    });
-
-    // Middleware here will only run if nothing was matched.
-
-    //Old stuff!
-      // app.UseMvc(routes =>
-      // {
-      //   routes.MapRoute(
-      //     name: "default",
-      //     template: "{controller=Home}/{action=Index}{id?}");
-      // });
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
 
       app.Run(async (context) =>
       {
